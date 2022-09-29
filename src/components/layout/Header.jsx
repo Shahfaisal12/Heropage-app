b0b7099fac6530131b9eb963e8deff3f38d6ca8f
shahfaisal12 <faisalmkd02@gmail.com>
2022-09-29 09:48:03 +0500

    boilerplate template

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,69 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-// import { Link } from "react-scroll";
-
-const linkstyle = { textDecoration: "none", color: "#000000" };
 
 const Header = () => {
   return (
     <>
-      <nav className="navbar fixed-top navbar-expand-lg shadow-sm">
-        <div className="container">
-          <a className="navbar-brand" href="/">
-            <img className="img-fluid" src="assests/logo-gnosis.png" alt="" style={{width:'50px'}} />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto mb-2 mb-lg-0" style={{marginLeft:'auto'}}>
-                <li className="nav-item">
-              <Link to="/" style={linkstyle}>
-                  <a className="nav-link active" aria-current="page" href="/">
-                    Protocol
-                  </a>
-              </Link>
-                </li>
-                <li className="nav-item">
-              <Link to="/faqs" style={linkstyle}>
-                  <a className="nav-link" href="/">
-                    Safe
-                  </a>
-              </Link>
-                </li>
-                <li className="nav-item">
-              <Link to="/faq" style={linkstyle}>
-                  <a className="nav-link" href="/">
-                  GnosisDAO
-                  </a>
-              </Link>
-                </li>
-                <li className="nav-item">
-              <Link to="/contact" style={linkstyle}>
-                  <a className="nav-link" href="/">
-                    Developers
-                  </a>
-              </Link>
-                </li>
-                <li className="nav-item">
-              <Link to="/contact" style={linkstyle}>
-                  <a className="nav-link" href="/">
-                    Careers
-                  </a>
-              </Link>
-                </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+     <h1>Header</h1>
     </>
   );
 };
--- src/components/routes/Routs.jsx
+++ src/components/routes/Routs.jsx
@@ -3,9 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Base from '../layout/Base';
 import Home from '../pages/Home';
-import Faq from '../pages/Faq';
-import Faqs from '../untils/Faqs';
-import Contact from '../pages/Contact';
 import PageNotFound from '../pages/PageNotFound';
 
 
@@ -16,9 +13,6 @@ const Routs = () => {
       <Base>
         <Routes>
           <Route index path='/' element={<Home />}/>
-          <Route path='/faqs' element={<Faqs />}/>
-          <Route path='/faq' element={<Faq />}/>
-          <Route path='/contact' element={<Contact />}/>
           <Route path='/*' element={<PageNotFound />}/>
         </Routes>
       </Base>
